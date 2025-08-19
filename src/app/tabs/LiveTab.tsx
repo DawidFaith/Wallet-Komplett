25d92e7a34451e12001ae316dca5a49c68d98555
Dawid311 <chojna.dawid1@gmail.com>
2025-08-19 08:43:28 +0000

    style: aktualisiere LiveTab durch Entfernen des Platzhalters und Hinzufügen eines Iframes für Live-Inhalte

--- src/app/tabs/LiveTab.tsx
+++ src/app/tabs/LiveTab.tsx
@@ -1,5 +1,12 @@
 export default function LiveTab() {
   return (
-    <div className="text-white text-xl">Live Bereich und XD</div>
+    <div className="w-full h-full">
+      <iframe 
+        src="https://bnds.us/9u4nop"
+        className="w-full h-[calc(100vh-80px)] border-0"
+        title="Live Auftritte"
+        allowFullScreen
+      />
+    </div>
   );
 }
